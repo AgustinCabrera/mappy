b4f20ba2d09042ab434ff64572d678fd2fe3c686
AgustinCabrera <agustinhkramer@gmail.com>
2022-05-11 13:19:38 -0300

    SeccondCommit

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-
 const options = {
   zoomControlOptions: {
-    position: google.maps.ControlPosition.RIGHT_CENTER // 'right-center' ,
-    // ...otherOptions
+    position:'right-center' ,
   }
 }
-
 function MyComponent() {
   const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: "YOUR_API_KEY" // ,
-    // ...otherOptions
+    googleMapsApiKey: process.env.REACT_APP_MAPKEY
   })
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+
 
   const renderMap = () => {
     // wrapping to a function is useful in case you want to access `window.google`
     // to eg. setup options or create latLng object, it won't be available otherwise
     // feel free to render directly if you don't need that
-    const onLoad = React.useCallback(
-      function onLoad (mapInstance) {
+    //const onLoad = React.useCallback(
+     // function onLoad (mapInstance) {
         // do something with map Instance
-      }
-    )
+    //  }
+   // )
     return <GoogleMap
       options={options}
-      onLoad={onLoad}
+      mapContainerStyle={containerStyle}
+       center={center}
+       zoom={10}
+   //   onLoad={onLoad}
     >
       {
         // ...Your map components
@@ -37,5 +46,9 @@ function MyComponent() {
     return <div>Map cannot be loaded right now, sorry.</div>
   }
 
-  return isLoaded ? renderMap() : <Spinner />
+  return isLoaded ? renderMap() :
+   <div>
+  algo
+  </div>
 }
+export default MyComponent 
