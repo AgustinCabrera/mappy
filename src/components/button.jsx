0b3c1ca5dc3b122449577e9e31a730eb0dbeab19
Agustin Cabrera <58793527+AgustinCabrera@users.noreply.github.com>
2023-03-07 14:13:08 -0300

    Visual Changes

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+function Button(props){
+    return <aside className='button'>
+      <button type ="button" class="button">
+      <span class="button__text">Add Field</span>
+      <span class="button__icon">
+        <ion-icon name="add-outline"></ion-icon>
+         </span>
+        </button>
+    </aside>
+    }
+    export default Button;
